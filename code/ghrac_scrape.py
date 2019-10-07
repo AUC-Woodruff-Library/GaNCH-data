e2c9421f2ed400b918108152a2feeaed079aaacf
Cliff Landis <clifflandis@clifflandis.net>
2019-10-07 15:54:04 -0400

    fixing scraper

--- code/ghrac_scrape.py
+++ code/ghrac_scrape.py
@@ -13,7 +13,6 @@ import urllib.request as urllib
 from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
-import unicodedata
 
 #create/open text file to write results to
 f = open("ghrac_data.txt","w+")
@@ -32,30 +31,19 @@ for i in range(100, 102):
     num = str(i)
     if name_box != None:  #test if there is an organization title on the page, if not, skip to else
         name = name_box.text.strip()  # strip out the starting and trailing tags
-        f.write("{'GHRAC RECORD NUMBER': '" + num + "', ")  #write page number as "record number"
+        f.write("{'RECORD NUMBER': '" + num + "', ")  #write page number as "record number"
         f.write("'ORGANIZATION': '" + name + "', ")  #write h2 tag contents as "organization"
     else:
         # f.write("{'RECORD NUMBER': '" + num + "', ") # write page number as "record number"
         # f.write(str(i) + " is an Empty Record" + "\n") # record is empty, record as such
         continue  # if the record is empty, just continue on to table contents
 
-#garbage below this point
 # grab the table
-    data = []
     table_data = soup.find('table')
-    rows = table_data.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])
 #for table_strip in table_data.children:
-    print(data)
-    #find = soup.find_all("strong", string=['CATEGORY', 'COUNTY', 'MAILING�ADDRESS'])
-    #print(find)
-    #print(table_data.strong.stripped_strings)
-    #results = table_data.get_text(",", strip=True) + "},\n"
-    results = data
-    results = str(results)
+    #find = soup.find_all("strong", string=['CATEGORY', 'COUNTY', 'MAILING ADDRESS'])
+    print(table_data.strong.stripped_strings)
+    results = table_data.get_text(",", strip=True) + "}\n"
     f.write(results)
 #    table_strip = table_data.text.strip()
 #print(table_strip)
